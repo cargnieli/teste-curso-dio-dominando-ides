eee6bcfa455ab2cf44cf18801b9a4452727f50cb
Walter Donizety Cargnieli <cargnieli@yahoo.com>
2021-08-10 10:07:35 -0300

    Teste

---
+++ src/com/local/java/PrimeiroPrograma.java
@@ -0,0 +1,27 @@
+package com.local.java;
+
+
+import model.Gato;
+
+public class PrimeiroPrograma {
+
+    public static void main(String[] args) {
+
+        Gato gato = new Gato();
+        System.out.println(gato);
+
+
+
+//
+//        int a = 5;
+//        int b = 3;
+//
+//        System.out.println("A soma de A + B dá: " + (a+b));
+
+    }
+
+}
+
+
+
+
