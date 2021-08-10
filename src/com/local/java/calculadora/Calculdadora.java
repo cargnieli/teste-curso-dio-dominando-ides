b1db08934c5f084fb6f341d77aa5f3592e42352c
Walter Donizety Cargnieli <cargnieli@yahoo.com>
2021-08-10 10:47:15 -0300

    Criando class livros

---
+++ src/com/local/java/calculadora/Calculdadora.java
@@ -0,0 +1,44 @@
+package com.local.java.calculadora;
+
+import java.util.Scanner;
+
+public class Calculdadora {
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        int a, b;
+
+        System.out.println("Digite o primeiro valor: ");
+        a = scan.nextInt();
+        System.out.println("Digite o segundo valor: ");
+        b = scan.nextInt();
+
+        int somar = somar(a,b);
+        int subtrair = subtrair(a,b);
+        int multiplicar = multiplicar(a,b);
+        int dividir = dividir(a,b);
+
+
+        System.out.println("Somar: " + somar);
+        System.out.println("Subtrair: " + subtrair);
+        System.out.println("Multiplicar: " + multiplicar);
+        System.out.println("dividir: " + dividir);
+
+
+    }
+          //Métodos
+    public static int somar(int a, int b) {
+        return a+b;
+    }
+    public static int subtrair(int a, int b) {
+        return a-b;
+    }
+    public static int multiplicar(int a, int b) {
+        return a*b;
+    }
+    public static int dividir(int a, int b) {
+        return a/b;
+    }
+
+}
